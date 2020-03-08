de098480540e21765914fd8c7c117d0fc10b2db6
Victoria Sharpe <victoriasharpe@Victorias-MacBook-Pro.local>
2020-03-08 15:03:48 -0400

    Updates

---
+++ precache-manifest.f45d034816c8ecd8a1705490ceec5865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ce2e1bf7d20c907eb2e",
+    "url": "/react-test1/static/css/main.6664f688.chunk.css"
+  },
+  {
+    "revision": "1ce2e1bf7d20c907eb2e",
+    "url": "/react-test1/static/js/main.54c3605b.chunk.js"
+  },
+  {
+    "revision": "28c060eb2aa88ac116f4",
+    "url": "/react-test1/static/js/runtime~main.2e7cc435.js"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/react-test1/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "0dcccd4266e12a4078af6c93e11bd1da",
+    "url": "/react-test1/index.html"
+  }
+];
